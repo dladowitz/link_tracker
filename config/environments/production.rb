3e17882e3ddc88cc185b2d3b26a3885d34811ad3
David Ladowitz <david@ladowitz.com>
2018-07-12 01:00:32 -0700

    add domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.domain = "https://coinbase-surveys.herokuapp.com"
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
